0106e9b1f1df7c6a90c72df4802c7166bb3815d1
Ahmed <ahmedmosa1121998@gmail.com>
2023-01-22 17:37:29 +0200

    done
    Done All

--- services/user.services.js
+++ services/user.services.js
@@ -29,13 +29,17 @@ module.exports.signin = async (req, res) => {
 
     const { email, password } = req.body
     let user = await UserModel.findOne({ email })
-    console.log(user);
+    console.log(user.password, "sssssssssss ", password);
+
     if (user) {
 
         const match = await bcrypt.compare(password, user.password)
         if (match) {
+            console.log(match);
+            console.log(user);
+            console.log(process.env.tokenSignature);
             const token = jwt.sign({ _id: user._id, name: user.name, isLoggIn: true },
-                process.env.tokenSignature, { expiresIn: 60 })
+                process.env.tokenSignature, { expiresIn: 60 * 60 * 12 })
             res.json({ mesage: "DONE Login ", token })
         } else {
             res.json({ message: "Incorrect pass " })
